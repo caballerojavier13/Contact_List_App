c13dd6b3bc9d1dd5c3f979bb4eb4a09ebb07af4d
sandeep <sandeep@vayu.com.au>
2017-01-16 16:16:04 +0545

    bootstrap updates

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,12 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+	
+	private
+
+		def set_permalink(hex_length)
+			self.permalink = loop do
+				random = SecureRandom.hex(hex_length)
+				break random unless self.class.exists?(permalink: random)
+			end
+		end  
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,7 @@
 class Contact < ApplicationRecord
 
+	before_create -> { set_permalink(2) }
+
 	def full_name
 		"#{self.first_name} #{self.last_name}".titleize
 	end
